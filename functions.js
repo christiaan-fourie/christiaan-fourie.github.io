7cb992d23aa4126d4ebe02771d6440a9705cf697
iam-krist <christiaanfourie60@gmail.com>
2022-02-21 22:35:27 +0200

    art section added

--- functions.js
+++ functions.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
     $( "#design__section" ).show();
     $( "#development__section" ).hide();
+    $( "#art__section" ).hide();
     $( "#design_tab" ).addClass('active');
 });
 
 // When Design Tab is clicked
 $( "#design_tab" ).click(function() {
     $( "#development__section" ).hide();
+    $( "#art__section" ).hide();
     $( "#design__section" ).show();
     $( "#design_tab" ).addClass('active');
     $( "#develop_tab" ).removeClass('active');
@@ -14,7 +16,18 @@ $( "#design_tab" ).click(function() {
   // When Develop Tab is clicked
 $( "#develop_tab" ).click(function() {
     $( "#development__section" ).show();
+    $( "#art__section" ).hide();
     $( "#design__section" ).hide();
     $( "#develop_tab" ).addClass('active');
     $( "#design_tab" ).removeClass('active');
+    $( "#art_tab" ).removeClass('active');
   });
+   // When Art Tab is clicked
+$( "#art_tab" ).click(function() {
+  $( "#development__section" ).hide();
+  $( "#design__section" ).hide();
+  $( "#art__section" ).show();
+  $( "#develop_tab" ).removeClass('active');
+  $( "#design_tab" ).removeClass('active');
+  $( "#art_tab" ).addClass('active');
+});

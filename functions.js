100aff4ed56aac7a401155e54074fbb50e2ae7fa
iam-krist <christiaanfourie60@gmail.com>
2022-03-24 10:09:54 +0200

    updates

--- functions.js
+++ functions.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
-    $( "#design__section" ).show();
-    $( "#development__section" ).hide();
+    $( "#about__section" ).show();
+    $( "#projects__section" ).hide();
     $( "#art__section" ).hide();
-    $( "#design_tab" ).addClass('active');
+    $( "#about_tab" ).addClass('active');
 });
 
-// When Design Tab is clicked
-$( "#design_tab" ).click(function() {
-    $( "#development__section" ).hide();
+// When About Tab is clicked
+$( "#about_tab" ).click(function() {
+    $( "#projects__section" ).hide();
     $( "#art__section" ).hide();
-    $( "#design__section" ).show();
-    $( "#design_tab" ).addClass('active');
-    $( "#develop_tab" ).removeClass('active');
+    $( "#about__section" ).show();
+    $( "#about_tab" ).addClass('active');
+    $( "#projects_tab" ).removeClass('active');
     $( "#art_tab" ).removeClass('active');
   });
   // When Develop Tab is clicked
-$( "#develop_tab" ).click(function() {
-    $( "#development__section" ).show();
+$( "#projects_tab" ).click(function() {
+    $( "#projects__section" ).show();
     $( "#art__section" ).hide();
-    $( "#design__section" ).hide();
-    $( "#develop_tab" ).addClass('active');
-    $( "#design_tab" ).removeClass('active');
+    $( "#about__section" ).hide();
+    $( "#projects_tab" ).addClass('active');
+    $( "#about_tab" ).removeClass('active');
     $( "#art_tab" ).removeClass('active');
   });
    // When Art Tab is clicked
 $( "#art_tab" ).click(function() {
-  $( "#development__section" ).hide();
-  $( "#design__section" ).hide();
+  $( "#projects__section" ).hide();
+  $( "#about__section" ).hide();
   $( "#art__section" ).show();
-  $( "#develop_tab" ).removeClass('active');
-  $( "#design_tab" ).removeClass('active');
+  $( "#projects_tab" ).removeClass('active');
+  $( "#about_tab" ).removeClass('active');
   $( "#art_tab" ).addClass('active');
 });
